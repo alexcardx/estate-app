34a35b5e469f3cfcc502bbbacafe0858ec0c28fb
alex <alex.miocard@gmail.com>
2025-07-21 16:35:12 +0300

    added interceptor

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -4,4 +4,24 @@ const apiRequest = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
 });
+
+apiRequest.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      const handleLogout = async () => {
+        try {
+          await apiRequest.post("/auth/logout");
+          localStorage.removeItem("user");
+          window.location.href = "/";
+        } catch (err) {
+          console.log(err);
+        }
+      };
+      handleLogout();
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default apiRequest;
